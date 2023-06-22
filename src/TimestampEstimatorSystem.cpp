6f5626cc6cc9a9e5ba6a16bcfb27f525172d19ca
Michal Rigan <m.rigan@sussex.ac.uk>
2023-06-22 06:42:42 -0500

    fixing division for clock freq

--- src/TimestampEstimatorSystem.cpp
+++ src/TimestampEstimatorSystem.cpp
@@ -19,7 +19,7 @@ TimestampEstimatorSystem::TimestampEstimatorSystem(uint64_t clock_frequency_hz) 
   : m_clock_frequency_hz(clock_frequency_hz)
 {
   TLOG_DEBUG(0) << "Clock frequency is " << m_clock_frequency_hz
-                << " clock_frequency_hz/1'000'000=" << (m_clock_frequency_hz / 1'000'000);
+                << " clock_frequency_hz/1'000'000=" << m_clock_frequency_hz/1000000.0;
 }
 
 dfmessages::timestamp_t
@@ -27,7 +27,7 @@ TimestampEstimatorSystem::get_timestamp_estimate() const
 {
   auto now = std::chrono::system_clock::now().time_since_epoch();
   auto now_us = std::chrono::duration_cast<std::chrono::microseconds>(now);
-  return (m_clock_frequency_hz / 1'000'000) * now_us.count();
+  return (m_clock_frequency_hz / 1000000.0) * now_us.count();
 }
 
 } // namespace timinglibs
