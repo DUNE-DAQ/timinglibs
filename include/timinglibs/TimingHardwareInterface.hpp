1821c955255a5ba181a5b95e7d67a9ab2b4a02f9
Stoyan Trilov <stoyan.trilov@cern.ch>
2023-09-15 12:56:28 +0100

    control dts hsi hw directly

---
+++ include/timinglibs/TimingHardwareInterface.hpp
@@ -0,0 +1,56 @@
+/**
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef TIMINGLIBS_INCLUDE_TIMINGLIBS_TIMINGHARDWAREINTERFACE_HPP_
+#define TIMINGLIBS_INCLUDE_TIMINGLIBS_TIMINGHARDWAREINTERFACE_HPP_
+
+
+#include "uhal/ConnectionManager.hpp"
+#include "uhal/ProtocolUDP.hpp"
+#include "uhal/log/exception.hpp"
+#include "uhal/utilities/files.hpp"
+#include <ers/Issue.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace dunedaq {
+namespace timinglibs {
+
+/**
+ * @brief TimingHardwareInterface sets up IPBus UHAL interface
+ */
+class TimingHardwareInterface
+{
+public:
+  /**
+   * @brief TimingHardwareInterface Constructor
+   * @param name Instance name for this TimingHardwareInterface instance
+   */
+  explicit TimingHardwareInterface();
+
+  TimingHardwareInterface(const TimingHardwareInterface&) = delete;            ///< TimingHardwareInterface is not copy-constructible
+  TimingHardwareInterface& operator=(const TimingHardwareInterface&) = delete; ///< TimingHardwareInterface is not copy-assignable
+  TimingHardwareInterface(TimingHardwareInterface&&) = delete;                 ///< TimingHardwareInterface is not move-constructible
+  TimingHardwareInterface& operator=(TimingHardwareInterface&&) = delete;      ///< TimingHardwareInterface is not move-assignable
+
+protected:
+  void configure_uhal(const nlohmann::json& obj);
+
+  std::string m_connections_file;
+  std::string m_uhal_log_level;
+  std::unique_ptr<uhal::ConnectionManager> m_connection_manager;
+//  std::unique_ptr<uhal::HwInterface> m_hsi_device;
+};
+} // namespace timinglibs
+} // namespace dunedaq
+
+#endif // TIMINGLIBS_INCLUDE_TIMINGLIBS_TIMINGHARDWAREINTERFACE_HPP_
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
---
+++ src/TimingHardwareInterface.cpp
@@ -0,0 +1,104 @@
+/**
+ * @file TimingHardwareInterface.cpp TimingHardwareInterface class
+ * implementation
+ *
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "TimingHardwareInterface.hpp"
+
+#include "timinglibs/hsireadout/Nljs.hpp"
+
+#include "timing/TimingIssues.hpp"
+
+#include "appfwk/DAQModuleHelper.hpp"
+#include "appfwk/app/Nljs.hpp"
+#include "logging/Logging.hpp"
+#include "rcif/cmd/Nljs.hpp"
+
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace dunedaq {
+namespace timinglibs {
+
+inline void
+resolve_environment_variables(std::string& input_string)
+{
+  static std::regex env_var_pattern("\\$\\{([^}]+)\\}");
+  std::smatch match;
+  while (std::regex_search(input_string, match, env_var_pattern)) {
+    const char* s = getenv(match[1].str().c_str());
+    const std::string env_var(s == nullptr ? "" : s);
+    input_string.replace(match[0].first, match[0].second, env_var);
+  }
+}
+
+TimingHardwareInterface::TimingHardwareInterface()
+  , m_connections_file("")
+  , m_connection_manager(nullptr)
+{
+}
+
+void
+TimingHardwareInterface::configure_uhal(const nlohmann::json& obj)
+{
+  TLOG_DEBUG(TLVL_ENTER_EXIT_METHODS) << get_name() << ": Entering do_configure() method";
+
+  if (obj.contains("connections_file")) {
+    m_connections_file =  obj["connections_file"];
+  }
+  else
+  {
+    // throw error
+  }
+
+  TLOG_DEBUG(0) << get_name() << "conf: con. file before env var expansion: " << m_connections_file;
+  resolve_environment_variables(m_connections_file);
+  TLOG_DEBUG(0) << get_name() << "conf: con. file after env var expansion:  " << m_connections_file;
+
+  if (obj.contains("uhal_log_level")) {
+    m_uhal_log_level =  obj["uhal_log_level"];
+  }
+  else
+  {
+    // throw error
+  }
+
+  if (!m_uhal_log_level.compare("debug")) {
+    uhal::setLogLevelTo(uhal::Debug());
+  } else if (!m_uhal_log_level.compare("info")) {
+    uhal::setLogLevelTo(uhal::Info());
+  } else if (!m_uhal_log_level.compare("notice")) {
+    uhal::setLogLevelTo(uhal::Notice());
+  } else if (!m_uhal_log_level.compare("warning")) {
+    uhal::setLogLevelTo(uhal::Warning());
+  } else if (!m_uhal_log_level.compare("error")) {
+    uhal::setLogLevelTo(uhal::Error());
+  } else if (!m_uhal_log_level.compare("fatal")) {
+    uhal::setLogLevelTo(uhal::Fatal());
+  } else {
+    throw InvalidUHALLogLevel(ERS_HERE, m_uhal_log_level);
+  }
+
+  try {
+    m_connection_manager = std::make_unique<uhal::ConnectionManager>("file://" + m_connections_file);
+  } catch (const uhal::exception::FileNotFound& excpt) {
+    std::stringstream message;
+    message << m_connections_file << " not found. Has TIMING_SHARE been set?";
+    throw UHALConnectionsFileIssue(ERS_HERE, message.str(), excpt);
+  }
+}
+
+} // namespace timinglibs
+} // namespace dunedaq
+
+// Local Variables:
+// c-basic-offset: 2
+// End:
