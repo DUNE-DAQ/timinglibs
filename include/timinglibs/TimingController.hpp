5041255d33aada8ad13edc9b1b268df6e9f511b2
Eric Flumerfelt <eflumerf@fnal.gov>
2025-02-11 10:58:35 -0600

    Use quotes for non-system Logging.hpp

--- include/timinglibs/TimingController.hpp
+++ include/timinglibs/TimingController.hpp
@@ -20,7 +20,7 @@
 
 #include "appfwk/DAQModule.hpp"
 #include "ers/Issue.hpp"
-#include <logging/Logging.hpp> // NOTE: if ISSUES ARE DECLARED BEFORE include logging/Logging.hpp, TLOG_DEBUG<<issue wont work.
+#include "logging/Logging.hpp" // NOTE: if ISSUES ARE DECLARED BEFORE include logging/Logging.hpp, TLOG_DEBUG<<issue wont work.
 #include "iomanager/Sender.hpp"
 #include "iomanager/Receiver.hpp"
 #include "utilities/WorkerThread.hpp"
